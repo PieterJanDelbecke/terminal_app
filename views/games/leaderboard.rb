42f34b12bc1fff39e76546131e0ca2555698da76
PieterJanDelbecke <pjdelbecke@gmail.com>
2021-09-29 16:56:15 +1000

    improving of leaderboard

--- views/games/leaderboard.rb
+++ views/games/leaderboard.rb
@@ -5,6 +5,10 @@ module Views
         def self.index(games)
             return puts "No games" if games.empty?
 
+            leader = leader games
+            puts "\nThe current leader is #{leader[1]}, with a score of #{leader[0]}"
+            puts
+
             headers = %w[player level stage platoons score]
             rows = table_rows_for games
             table = TTY::Table.new headers, rows
@@ -16,5 +20,17 @@ module Views
                     [game.name, game.level, game.stage, game.platoons, game.score]
                 end
         end
+
+        def self.leader(games)
+            leader = [0,""]
+            games.each do |game|
+                if game.score > leader[0]
+                    leader[0] = game.score
+                    leader[1] = game.name
+                end
+            end
+            leader
+        end
+
     end
 end
